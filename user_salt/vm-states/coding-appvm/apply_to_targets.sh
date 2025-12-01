4f5f1b3c5b12556a55cc2f3c25821370f1cf43bc
wamu_M <contact@wamu-m.com>
2025-12-01 14:30:34 +0100

    Create base for appvm creation scripts
    Some base to automatically create app vms following "stereotypes"

--- user_salt/vm-states/coding-appvm/apply_to_targets.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python3
-# SPDX-License-Identifier: Apache-2.0
-# SPDX-FileCopyrightText: 2025 Marcel M. T. (wamu_M) <contact@wamu-m.com>
-
-with open('targets.conf', 'r') as file:
-	
-
-
